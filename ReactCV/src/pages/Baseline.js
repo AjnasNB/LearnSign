1b3cb4e7c28a0a3d157f02b30c22375094226f22
AjnasNB <ajnasnb@gmail.com>
2024-04-07 16:33:46 +0530

    update

--- ReactCV/src/pages/Baseline.js
+++ ReactCV/src/pages/Baseline.js
@@ -18,7 +18,7 @@ function AllClasses() {
       }
       wordBank={wordBank}
       modelUrl={
-        "https://raw.githubusercontent.com/AjnasNB/LearnSign/main/ReactCV/src/tfjs_model_mobilenetv2_fpnlite_all_classes_v2/model.json"
+        "../fun/model.json"
       }
     />
   );
